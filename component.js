3f63424a452ce197a02d88afdd7a5a1cba8675a6
GootrolHuh <105822763+GootrolHuh@users.noreply.github.com>
2022-05-23 17:10:58 -0300

    Add files via upload

---
+++ component.js
@@ -0,0 +1,7 @@
+let string1 = "Been...";
+
+document.getElementByid('string1').onclick = () => {
+    string1 = text.toLowercase(document.getElementByid("aaa1"))
+    document.getElementByid('selected1').innerText = string1;
+};
+
